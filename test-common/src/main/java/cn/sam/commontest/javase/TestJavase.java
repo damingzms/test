f73743f231b083790dcf63a05a32900282c5ff38
damingzms <damingzms@126.com>
2016-09-30 11:38:37 +0800

    >test-common

--- test-common/src/main/java/cn/sam/commontest/javase/TestJavase.java
+++ test-common/src/main/java/cn/sam/commontest/javase/TestJavase.java
@@ -1,8 +1,19 @@
 package cn.sam.commontest.javase;
 
 public class TestJavase {
-	public static void main(String[] args) {
+	
+	public static void test1() {
 		System.out.println((Integer)128 == (Integer)128);
+	}
+	
+	public static void test2() {
+		Double d = null;
+		System.out.println(d == null ? 2D : d + d);	// 2.0
+		System.out.println((d == null ? 2D : d) + d);	// NullPointerException
+	}
+	
+	public static void main(String[] args) {
+		test2();
 	}
 
 }
