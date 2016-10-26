7d644cee0bce94f38a7fe3c224abe87271b860bb
damingzms <damingzms@126.com>
2016-10-26 13:48:03 +0800

    测试dozer；测试springboot

--- test-common/src/main/java/cn/sam/commontest/beanutils/dozer/Test.java
+++ test-common/src/main/java/cn/sam/commontest/beanutils/dozer/Test.java
@@ -1,16 +1,42 @@
 package cn.sam.commontest.beanutils.dozer;
 
+import java.util.ArrayList;
+
 import org.dozer.DozerBeanMapper;
 
-public class Test {
+import cn.sam.commontest.beanutils.bean.Apple;
+import cn.sam.commontest.beanutils.bean.Orange;
 
-	public static void main(String[] args) {
+public class Test {
+	public static final DozerBeanMapper MAPPER = new DozerBeanMapper();
+	
+	public static void test1() {
 		Father f = new Father();
 		f.setJob("engineer");
 		
-		DozerBeanMapper mapper = new DozerBeanMapper();
-		Son son = mapper.map(f, Son.class);
-		System.out.println(son.getJob());
+		Son son = MAPPER.map(f, Son.class);
+		System.out.println(son.getJob());	// engineer
+	}
+	
+	/**
+	 * deep copy, 而且只是泛型
+	 */
+	public static void test2() {
+		Apple a = new Apple();
+		a.setColor("red");
+		a.setOranges(new ArrayList<Orange>());
+		
+		Orange o = new Orange();
+		o.setColor("blue");
+		a.getOranges().add(o);
+		System.out.println(o);
+		
+		Apple a2 = MAPPER.map(a, Apple.class);
+		System.out.println(a2.getOranges().get(0));
+	}
+
+	public static void main(String[] args) {
+		test2();
 	}
 
 }
--- test-springboot/src/main/java/cn/sam/test/springboot/Application.java
+++ test-springboot/src/main/java/cn/sam/test/springboot/Application.java
@@ -1,21 +1,37 @@
 package cn.sam.test.springboot;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@RequestMapping("/")
 public class Application {
+	protected final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	
-	@Value("${name}")
-	private String name;
-
-	@RequestMapping("/")
-	String home() {
-		return String.format("Hello %s!", name);
+	@RequestMapping("")
+	@ResponseBody
+	public Object operate(String operand1, String operand2) {
+		String result = null;
+		if (StringUtils.isBlank(operand1) || StringUtils.isBlank(operand2)) {
+			result = "Parameters [operand1] and [operand2] can not be blank.";
+			return result;
+		}
+		try {
+			double op1d = Double.valueOf(operand1);
+			double op2d = Double.valueOf(operand2);
+			result = String.valueOf(op1d + op2d);
+		} catch (Exception e) {
+			LOGGER.info("not a number.");
+			result = operand1 + operand2;
+		}
+		return result;
 	}
 
 	public static void main(String[] args) throws Exception {
