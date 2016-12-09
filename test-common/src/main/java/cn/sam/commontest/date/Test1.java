72c103fd803aef5437a03d0b738632fb414c60d6
nb <nb@nb12984.biostime.com>
2016-12-09 10:35:12 +0800

    date

--- test-common/src/main/java/cn/sam/commontest/date/Test1.java
+++ test-common/src/main/java/cn/sam/commontest/date/Test1.java
@@ -1,18 +1,27 @@
 package cn.sam.commontest.date;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.apache.commons.lang3.time.DateUtils;
 
 public class Test1 {
-
-	public static void main(String[] args) {
+	
+	public static void test1() {
 		try {
 			System.out.println(DateUtils.parseDate("2016-09-7 15:00:00", "yyyy-MM-dd HH:mm:ss"));
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void test2() {
+		String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(null);	// NullPointerException
+		System.out.println(format);
+	}
 
+	public static void main(String[] args) {
+		test2();
 	}
 
 }
