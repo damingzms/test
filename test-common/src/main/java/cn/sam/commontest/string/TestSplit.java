3540cf5e11f0a00ccc8915a4619a58d32f166c7a
Samuel <Samuel@V12984.biostime.com>
2017-11-24 10:44:52 +0800

    test

--- test-common/src/main/java/cn/sam/commontest/string/TestSplit.java
+++ test-common/src/main/java/cn/sam/commontest/string/TestSplit.java
@@ -1,9 +1,18 @@
 package cn.sam.commontest.string;
 
 public class TestSplit {
+	
+//	public static void main(String[] args) {
+//		String str = "asaa";
+//		String[] split = str.split(";");
+//		System.out.println(split.length);
+//	}
+	
 	public static void main(String[] args) {
-		String str = "asaa";
-		String[] split = str.split(";");
+		String str = "123:1234|111:222";
+		String[] split = str.split("[|]");
 		System.out.println(split.length);
+		System.out.println(split[0].split(":").length);
 	}
+	
 }
