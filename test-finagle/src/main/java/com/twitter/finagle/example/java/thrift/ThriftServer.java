c3625149384bde5e41d30b33570444e0edc18b77
damingzms <damingzms@126.com>
2018-01-03 00:25:37 +0800

    finagle

--- test-finagle/src/main/java/com/twitter/finagle/example/java/thrift/ThriftServer.java
+++ test-finagle/src/main/java/com/twitter/finagle/example/java/thrift/ThriftServer.java
@@ -1,28 +1,32 @@
 package com.twitter.finagle.example.java.thrift;
 
-import java.net.InetSocketAddress;
-
 import com.twitter.finagle.ListeningServer;
 import com.twitter.finagle.Thrift;
 import com.twitter.finagle.example.thriftjava.Hello;
-//import com.twitter.finagle.example.thriftjava.Hello;
 import com.twitter.util.Await;
+import com.twitter.util.Future;
 import com.twitter.util.TimeoutException;
 
+/**
+ * 需要设置scrooge-maven-plugin插件，根据IDL（*.thrift）生成java代码。见pom.xml文件
+ *
+ * Scrooge is meant to be a replacement for the Apache Thrift code generator, and generates conforming, binary-compatible codecs by building on top of libthrift.
+ * 为Scrooge设置--finagle参数，可以根据IDL生成finagle的thrift协议的代码。见pom.xml文件
+ */
 public final class ThriftServer {
 
-  private ThriftServer() { }
+	private ThriftServer() {
+	}
 
-  /**
-   * Runs the example with given {@code args}.
-   *
-   * @param args the argument list
-   */
-  public static void main(String[] args) throws TimeoutException, InterruptedException {
-    //#thriftserverapi
-    Hello.ServiceIface impl = new HelloImpl();
-    ListeningServer server = Thrift.server().serveIface(new InetSocketAddress(8080), impl);
-    Await.ready(server);
-    //#thriftserverapi
-  }
+	public static class HelloImpl implements Hello.ServiceIface {
+		public Future<String> hi() {
+			return Future.value("hi");
+		}
+	}
+
+	public static void main(String[] args) throws TimeoutException, InterruptedException {
+		Hello.ServiceIface impl = new HelloImpl();
+		ListeningServer server = Thrift.server().serveIface("localhost:8080", impl);
+		Await.ready(server);
+	}
 }
--- test-finagle/src/main/java/com/twitter/finagle/example/scala/thrift_deprecated/ThriftClient.java
+++ test-finagle/src/main/java/com/twitter/finagle/example/scala/thrift_deprecated/ThriftClient.java
@@ -1,9 +1,9 @@
-package com.twitter.finagle.example.java.thrift;
+package com.twitter.finagle.example.scala.thrift_deprecated;
 
 import scala.runtime.BoxedUnit;
 
 import com.twitter.finagle.Thrift;
-import com.twitter.finagle.example.thriftjava.Hello;
+import com.twitter.finagle.example.thriftscala.Hello;
 import com.twitter.util.Await;
 import com.twitter.util.Function;
 import com.twitter.util.Future;
@@ -14,7 +14,7 @@ public final class ThriftClient {
 	}
 
 	public static void main(String[] args) throws Exception {
-		Hello.ServiceIface client = Thrift.client().newIface("localhost:8080", Hello.ServiceIface.class);
+		Hello.FutureIface client = Thrift.client().newIface("localhost:8080", Hello.FutureIface.class);
 		Future<String> response = client.hi().onSuccess(new Function<String, BoxedUnit>() {
 			@Override
 			public BoxedUnit apply(String response) {
