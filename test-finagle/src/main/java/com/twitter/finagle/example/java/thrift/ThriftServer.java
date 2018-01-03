23f587093493acfe2328103b09f58bf5485b84f7
damingzms <damingzms@126.com>
2018-01-04 00:19:16 +0800

    test finagle

--- test-finagle/src/main/java/com/twitter/finagle/example/java/thrift/ThriftServer.java
+++ test-finagle/src/main/java/com/twitter/finagle/example/java/thrift/ThriftServer.java
@@ -9,14 +9,14 @@ import com.twitter.util.TimeoutException;
 
 /**
  * 需要设置scrooge-maven-plugin插件，根据IDL（*.thrift）生成java代码。见pom.xml文件
- *
- * Scrooge is meant to be a replacement for the Apache Thrift code generator, and generates conforming, binary-compatible codecs by building on top of libthrift.
+ * <p>
+ * Scrooge is meant to be a replacement for the Apache Thrift code generator, and generates conforming, binary-compatible codecs by building on top of libthrift.<br>
  * 为Scrooge设置--finagle参数，可以根据IDL生成finagle的thrift协议的代码。见pom.xml文件
- * 
+ * <p>
  * Scrooge对required/optional/unspecified-requiredness and default values的具体处理：https://twitter.github.io/scrooge/Semantics.html
- * 
+ * <p>
  * IDL（*.thrift）文件规范，以及使用Scrooge thrift linter工具检查文件内容是否符合规范：https://twitter.github.io/scrooge/Linter.html
- * 
+ * <p>
  * Scrooge与Finagle的整合：https://twitter.github.io/scrooge/Finagle.html
  * 
  */
