622ce3c76ecb049c8c988cc1cd83fc7393549732
user <user@S12984.biostime.com>
2018-08-29 13:45:38 +0800

    common

--- test-common/src/main/java/cn/sam/commontest/collection/Test1.java
+++ test-common/src/main/java/cn/sam/commontest/collection/Test1.java
@@ -1,15 +1,29 @@
 package cn.sam.commontest.collection;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class Test1 {
-
-	public static void main(String[] args) {
+	
+	public static void test1() {
 		List<Long> ids = new ArrayList<>();
 		System.out.println(String.format("test: %s", StringUtils.join(ids.toArray(), ", ")));
+	}
+	
+	public static void test2() {
+		Map<String, Object> map = new HashMap<>();
+		map.put(null, "1");
+		map.put("key1", 2);
+		System.out.println(map.get(null));
+		System.out.println(map.get("key1"));
+	}
+
+	public static void main(String[] args) {
+		test2();
 	}
 
 }
