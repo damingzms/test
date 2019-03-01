9b46e393f6e99e3998da9c25a9f56e7d2a4962ff
damingzms <damingzms@126.com>
2019-03-01 16:04:47 +0800

    annotation

---
+++ test-common/src/main/java/cn/sam/commontest/javase/annotation/TestAnnotation.java
@@ -0,0 +1,12 @@
+package cn.sam.commontest.javase.annotation;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class TestAnnotation {
+
+	public static void main(String[] args) {
+		Class<PostMapping> c = PostMapping.class;
+
+	}
+
+}
